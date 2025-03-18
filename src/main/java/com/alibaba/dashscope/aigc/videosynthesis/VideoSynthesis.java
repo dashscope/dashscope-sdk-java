6b259f91c054dea21871d5de83b4a08df72f1406
zm <zm02074348@alibaba-inc.com>
2025-03-18 13:24:33 +0800

    Fixed video generation prompt as non-required and added two i2v model names

--- src/main/java/com/alibaba/dashscope/aigc/videosynthesis/VideoSynthesis.java
+++ src/main/java/com/alibaba/dashscope/aigc/videosynthesis/VideoSynthesis.java
@@ -30,6 +30,8 @@ public final class VideoSynthesis {
     public static final String WANX_IMG_TO_VIDEO_PRO = "wanx-img2video-pro";
     public static final String WANX_2_1_T2V_PLUS = "wanx2.1-t2v-plus";
     public static final String WANX_2_1_T2V_TURBO = "wanx2.1-t2v-turbo";
+    public static final String WANX_2_1_I2V_PLUS = "wanx2.1-i2v-plus";
+    public static final String WANX_2_1_I2V_TURBO = "wanx2.1-i2v-turbo";
   }
 
   /** Video synthesis size */
