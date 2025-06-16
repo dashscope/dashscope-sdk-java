22416e04562aed54fcb661621a76f7bf6d79b9d0
挪麦 <lengjiayi.ljy@alibaba-inc.com>
2025-06-16 11:24:21 +0800

    [Add] set pass through to recv task-started in callback, add first package and first audio timeout for cosyvoice

---
+++ src/main/java/com/alibaba/dashscope/exception/DashscopeTimeoutException.java
@@ -0,0 +1,9 @@
+// Copyright (c) Alibaba, Inc. and its affiliates.
+
+package com.alibaba.dashscope.exception;
+
+public class DashscopeTimeoutException extends Exception {
+  public DashscopeTimeoutException(String msg) {
+    super(msg);
+  }
+}
