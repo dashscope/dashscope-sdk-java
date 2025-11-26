ad053c5eb664d02862f030c39d77b35bde0ad7e9
遥仙 <songsong.sss@alibaba-inc.com>
2025-11-26 14:57:44 +0800

    feat(model/qwen3-livetranslate): add param corpus.phrases

--- src/main/java/com/alibaba/dashscope/audio/omni/OmniRealtimeTranslationParam.java
+++ src/main/java/com/alibaba/dashscope/audio/omni/OmniRealtimeTranslationParam.java
@@ -4,10 +4,20 @@ package com.alibaba.dashscope.audio.omni;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.HashMap;
+
 /** @author songsong.shao */
 @Builder
 @Data
 public class OmniRealtimeTranslationParam {
     /** language for translation */
     private String language;
+    private Corpus corpus;
+    @Builder
+    @Data
+    public static class Corpus {
+        /** corpus for qwen-asr-realtime */
+        private HashMap<String, Object> phrases; // translation phrases,
+    }
+
 }
