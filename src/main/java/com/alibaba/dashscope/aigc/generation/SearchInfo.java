cb0441d473f8d9aaa6efca206d4eebaafc597ddd
xiongbinbin <xiongbinbin.xbb@alibaba-inc.com>
2026-04-07 11:45:31 +0800

    compatibility handling

---
+++ src/main/java/com/alibaba/dashscope/aigc/generation/SearchInfo.java
@@ -0,0 +1,10 @@
+package com.alibaba.dashscope.aigc.generation;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+/** @deprecated 请使用 {@link com.alibaba.dashscope.common.SearchInfo} */
+@Deprecated
+@SuperBuilder
+@Data
+public class SearchInfo extends com.alibaba.dashscope.common.SearchInfo {}
---
+++ src/main/java/com/alibaba/dashscope/aigc/generation/SearchOptions.java
@@ -0,0 +1,10 @@
+package com.alibaba.dashscope.aigc.generation;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+/** @deprecated 请使用 {@link com.alibaba.dashscope.common.SearchOptions} */
+@Deprecated
+@SuperBuilder
+@Data
+public class SearchOptions extends com.alibaba.dashscope.common.SearchOptions {}
