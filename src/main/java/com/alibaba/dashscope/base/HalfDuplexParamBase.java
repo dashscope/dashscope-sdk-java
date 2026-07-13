6e83e47a4e8c8cc7391a7dce2c5f23d41d629363
kevin <lu384090@gmail.com>
2026-07-13 14:55:39 +0800

    Revert "fix: initialize parameters and headers maps in HalfDuplexParamBase to prevent NPE"
    This reverts commit d667ac8e886a2e30a09a1c534e7da8a07fa3df55.

--- src/main/java/com/alibaba/dashscope/base/HalfDuplexParamBase.java
+++ src/main/java/com/alibaba/dashscope/base/HalfDuplexParamBase.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Singular;
 import lombok.experimental.SuperBuilder;
 
 /** The user input and parameter. */
@@ -28,7 +27,7 @@ public abstract class HalfDuplexParamBase {
   @GsonExclude @Builder.Default private Boolean enableEncrypt = false;
 
   /** The extra parameters. */
-  @GsonExclude @Singular protected Map<String, Object> parameters = new HashMap<>();
+  @GsonExclude @Builder.Default protected Map<String, Object> parameters = new HashMap<>();
 
   public abstract String getModel();
   /**
@@ -39,7 +38,7 @@ public abstract class HalfDuplexParamBase {
   public abstract Map<String, Object> getParameters();
 
   /** The custom http header. */
-  @GsonExclude @Singular protected Map<String, Object> headers = new HashMap<>();
+  @GsonExclude @Builder.Default protected Map<String, Object> headers = new HashMap<>();
 
   /**
    * The custom http header.
